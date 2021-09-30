aa6f34594be449dcb3d9770ed53ffc29ddfe0ce2
VenGallardo <letskeddotcom@gmail.com>
2021-09-30 12:13:52 +0800

    added the bg color transitions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import react, {useState} from 'react';
 
-function App() {
+const App = () =>{
+
+  const [color, setColor] = useState("#FFFFF");
+
+  const handleColor = (e) =>{
+    setColor(e.target.id);
+    // console.log(color)
+  }
+  const handleDefaultColor = (e) =>{
+    setColor(e.target.id)
+  }
+
+  const appStyle = {
+    backgroundColor:(color),
+    transition: 'all 1s ease',
+  }
+
   return (
-    <div className="App">
-      Sample
+    <div id="white" onMouseOver={handleDefaultColor} style={appStyle} className="App">
+      
+      <div id="yellow" onMouseOver={handleColor} className="boxes"></div>
+      <div id="gray" onMouseOver={handleColor} className="boxes"></div>
+      <div id="blue" onMouseOver={handleColor} className="boxes"></div>
+      <div id="green" onMouseOver={handleColor} className="boxes"></div>
     </div>
   );
 }
